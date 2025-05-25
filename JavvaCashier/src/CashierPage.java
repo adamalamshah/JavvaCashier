eed96797dd15340e5a2b44bb1d46de429b6729bc
Adam Alamshah <adamalamsh@gmail.com>
2025-05-26 01:40:32 +0700

    tabel

--- JavvaCashier/src/CashierPage.java
+++ JavvaCashier/src/CashierPage.java
@@ -37,6 +37,7 @@ public class CashierPage extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jPanel5 = new javax.swing.JPanel();
         jTextField3 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
@@ -153,6 +154,20 @@ public class CashierPage extends javax.swing.JFrame {
 
         jScrollPane1.setBackground(new java.awt.Color(239, 239, 239));
         jScrollPane1.setForeground(new java.awt.Color(239, 239, 239));
+        jScrollPane1.setFont(new java.awt.Font("Poppins", 0, 12)); // NOI18N
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "ID produk", "Nama produk", "Harga satuan", "Qty", "Subtotal"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         jPanel5.setBackground(new java.awt.Color(56, 76, 120));
         jPanel5.setPreferredSize(new java.awt.Dimension(250, 33));
@@ -315,7 +330,7 @@ public class CashierPage extends javax.swing.JFrame {
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -405,6 +420,7 @@ public class CashierPage extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
